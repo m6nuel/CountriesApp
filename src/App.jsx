cc5f3f3d9a5243941c6d822104e5c6ae8d1663a1
Jesus M Delgado D <m6nuel@gmail.com>
2023-10-06 19:44:46 -0300

    intentando reset de filtros

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 import './App.css'
 import Aside from './components/Aside'
 import Countries from './components/Countries'
-// import { countriess } from '../src/service/countries'
 import { useFilter } from './hooks/useFilter'
 import Footer from './components/Footer'
 import Header from './components/Header'
-// import { useContext } from 'react'
-// import { FiltersContext } from './context/filters'
 
 function App () {
   const { filters, filterContinents, ordenAlpha } = useFilter()
   const continentsFiltered = filterContinents(filters.listApp)
   const continentsOrdered = ordenAlpha(continentsFiltered)
-  // const { filters } = useContext(FiltersContext)
 
   const cfiltSearch = filterContinents(filters.search)
   const cfilOrdered = ordenAlpha(cfiltSearch)
